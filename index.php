68b58cc58b28a746c0f9252099a8b8e034f602a0
Whispmoon <tbilliau@gmail.com>
2017-02-27 22:05:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Developpeur WEB & Logiciel</title>
+    <meta name="description" content="developpeur logiciel et application: html, css, php, javascript, git, node.js, bootstrap, phonegap, wordpress, symfony, sql, uml, mongodb">
+    <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="css/styles_print.css" media="print" />
+    <link href="font/Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="icon" type="image/png" href="img/profil.png" />
+     <!-- Hotjar Tracking Code for www.thomasbilliau.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:411645,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+</head>
+
+<body>
+    <header>
+        <h1>DEVELOPPEUR WEB & LOGICIEL</h1>
+            <div class="contact">
+                <h2>Thomas BILLIAU</h2>
+                <ul>
+                    <li>44100 Nantes</li>
+                    <li><a href="tel:+33669358915" title="Appeler">06.69.35.89.15</a></li>
+                    <li><a href="mailto:tbilliau.dev@gmail.com" title="Envoyer un email">tbilliau.dev@gmail.com</a></li>
+                    <li>36 ans</li>
+                    <li>Permis B</li>
+                    <li><a href="http://www.thomasbilliau.com">www.thomasbilliau.com</a></li>
+                    <li><a href="https://github.com/whispmoon">Mon github</a></li>
+                </ul>
+            </div>
+        <div class="photo">
+            <img src="img/profil.png" alt="photoProfil">
+        </div>
+        <div class="aboutMe">
+            <p>En recherche de stage du 10 avril au 13 juillet 2017.</p>
+            <p>Passionné d'informatique et de High-tech j'ai mené tout d'abord une partie de ma carrière en tant que commercial
+                 dans ce secteur (Microsoft, Belkin, Konami). Aujourd'hui je me suis réorienté vers le développement.</p>
+        </div>
+    </header>
+    <main>
+        <nav>
+            <div>
+                <a href="mailto:tbilliau.dev@gmail.com" title="Envoyer un email"><img src="img/email.png" alt="email"></a>
+            </div>
+            <div>
+                <a href="tel:+33669358915" title="Appeler"><img src="img/phone.png" alt="call"></a>
+            </div>
+            <div>
+                <a href="#formation" title="formations"><img src="img/school.png" alt="formation"></a>
+            </div>
+            <div>
+                <a href="#work" title="Expériences"><img src="img/work2.png" alt="work"></a>
+            </div>
+            <div>
+                <a href="#hobbies" title="Centres d'intérets"><img src="img/xbox.png" alt="hobbies"></a>
+            </div>
+            <div>
+                <a href="http://www.linkedin.com/in/thomasbilliau" title="linkedin"><img src="img/linkedin.png" alt="linkedin"></a>
+            </div>
+            <div>
+                <a href="https://twitter.com/ThomasBILLIAU" title="twitter"><img src="img/twitter.png" alt="twitter"></a>
+            </div>
+            <!--<div>
+                <a href="https://www.instagram.com/thomas_billiau_/"><img src="img/instagram.png" alt="instagram"></a>
+            </div>-->
+        </nav>
+        <div class="container">
+            <div class="formation" id="formation">
+                <h2>FORMATIONS</h2>
+                <ul>
+                    <li>
+                        <h3><strong>Développeur Logiciel</strong></h3>
+                        <p>IMIE NANTES - 2016 /2017 :</p>
+                        <p>
+                            Piloter un projet, génie Logiciel, développer des applications (PHP, HTML5, JS, PhoneGap)
+                            , persistances des données (Symfony 2), concevoir et mettre en place une base de données
+                        </p>
+                    </li>
+                    <li>
+                        <h3><strong>DAEU</strong></h3>
+                        <p>NANTES 2015/2016 : Bac
+                        </p>
+                    </li>
+                    <li>
+                        <h3><strong>BEP Vente Action Marchande</strong></h3>
+                        <p>GRETA NANTES 1999/2000</p>
+                    </li>
+                </ul>
+            </div>
+        <div class="jobs" id="work">
+            <h2>EXPERIENCES</h2>
+                <ul>
+                    <li>
+                        <h3><strong> World  Trade and Technologies :</strong></h3>
+                        <p>2011-2015 : 4 ans Chef de secteur :</p>
+                        <p>En charge du développement des ventes (secteur High/Tech) sur un parc de 110 magasins Leclerc (Région Bretagne/Pays De Loire et Sud-Ouest).</p>
+                    </li>
+                    <li>
+                        <h3><strong>Microsoft France :</strong></h3>
+                        <p>2009-2011 : 3 ans Chef de secteur :</p>
+                        <p> En charge du marché console et jeux XBOX sur un parc de 130 magasins (CRF/Auchan/Leclerc/Hyper U/Boulanger/Darty/Fnac).</p>
+                    </li>
+                    <li>
+                        <h3><strong>Konami, ViaMichelin,  Tele Atlas:</strong></h3>
+                        <p>2006-2009 : 3 ans Chef de secteur</p>
+                    </li>
+                    <li>
+                        <h3><strong>Garmin :</strong></h3>
+                        <p>2006 : 4 mois Chef de secteur</p>
+                    </li>
+                    <li>
+                        <h3><strong>Lexmark, Belkin :</strong></h3>
+                        <p>2004-2005 : 1 an Chef de secteur</p>
+                    </li>
+                    <li>
+                        <h3><strong>Carrefour France :</strong></h3>
+                        <p>2001-2004 : 3 ans, Vendeur EPCS</p>
+                    </li>
+                    <li>
+                        <h3><strong>Darty :</strong></h3>
+                        <p>2000-2001 : Nantes 1an, Vendeur Informatique</p>
+                    </li>
+                </ul>
+        </div>
+            <div class="hobbies" id="hobbies">
+                <h2>CENTRES D'INTERETS</h2>
+                <div class="picture">
+                    <ul>
+                        <li>Vélo<img src="img/Bike3.png" alt="velo"></li>
+                        <li>Podcast<img src="img/podcast3.jpg" alt="podcast"></li>
+                        <li>Running<img src="img/running2.png" alt="running"></li>
+                        <li>Kindle<img src="img/kindle4.png" alt="kindle"></li>
+                        <li>Skateboard<img src="img/skateboard.png" alt="skateboard"></li>
+                    </ul>   
+                </div>
+            </div>
+        </div>
+
+        <div class="aside">
+            <div class="atouts">
+                <h2>COMPETENCES</h2> 
+                <ul>  
+                <?php 
+                $advantages = ["Marketing","Former","Dynamique","Early-adopter"
+                ,"Négocier","Reporting","High-tech","Autonome","Evangéliser","Challenge",
+                "Prescripteur","Analyse","Pédagogue","Travail en équipe"]; 
+                for ($i=0; $i < 14; $i++) { 
+                echo"<li>".$advantages[$i]."</li>";
+                }
+                ?>
+                </ul>
+            </div>
+        <dl class="skills-list">
+            <dt>HTML:</dt>
+                <dd class="skill-65" title="HTML" ><div></div></dd>
+            <dt>CSS:</dt>
+                <dd class="skill-65" title="CSS" ><div></div></dd>
+            <dt>PHP:</dt>
+                <dd class="skill-50" title="PHP" ><div></div></dd>
+            <dt>JAVASCRIPT:</dt>
+                <dd class="skill-35" title="JAVASCRIPT" ><div></div></dd>
+            <dt>GIT:</dt>
+                <dd class="skill-70" title="GIT" ><div></div></dd>
+            <dt>NODE.JS:</dt>
+                <dd class="skill-30" title="NODE.JS" ><div></div></dd>
+            <dt>BOOTSTRAP:</dt>
+                <dd class="skill-60" title="BOOTSTRAP" ><div></div></dd>
+            <dt>PHONEGAP:</dt>
+                <dd class="skill-30" title="PHONEGAP" ><div></div></dd>
+            <dt>WORDPRESS:</dt>
+                <dd class="skill-50" title="WORDPRESS" ><div></div></dd>
+            <dt>SYMFONY:</dt>
+                <dd class="skill-40" title="SYMFONY" ><div></div></dd>
+            <dt>SQL:</dt>
+                <dd class="skill-50" title="SQL" ><div></div></dd>
+            <dt>UML:</dt>
+                <dd class="skill-45" title="UML" ><div></div></dd>
+            <dt>MERISE:</dt>
+                <dd class="skill-45" title="MERISE" ><div></div></dd>
+            <dt>MONGODB:</dt>
+                <dd class="skill-30" title="MONGODB" ><div></div></dd>
+
+        </dl>
+
+        </div>
+    </main>
+    <footer>
+        
+    </footer>
+    <div class="googleAnalytics" title="googleAnalytics">
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90352201-1', 'auto');
+  ga('send', 'pageview');
+</script>    
+    </div>
+    <script src="js/app.js">
+        
+    </script>
+   
+
+</body>
+
+</html>
